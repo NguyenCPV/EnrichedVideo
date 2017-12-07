fff5b8a2ed0ea5aeca4e17542199c923455ffd27
Bechet <yuukikune@gmail.com>
2017-12-07 10:28:47 +0100

    Full screen

--- app/src/main/java/fr/enssat/lnfl/enrichedvideo/MainActivity.java
+++ app/src/main/java/fr/enssat/lnfl/enrichedvideo/MainActivity.java
@@ -2,12 +2,17 @@ package fr.enssat.lnfl.enrichedvideo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Remove the notification bar (full screen)
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
     }
 }
