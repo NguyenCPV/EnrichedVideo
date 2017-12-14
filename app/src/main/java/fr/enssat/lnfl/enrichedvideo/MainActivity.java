7094b0f135c0fab47b41cde2efa260240b76ee1f
NguyenCPV <pnguyen@enssat.fr>
2017-12-14 08:39:42 +0100

    Merge branch 'master' of C:\Users\cypri\AndroidStudioProjects\EnrichedVideo with conflicts.

--- app/src/main/java/fr/enssat/lnfl/enrichedvideo/MainActivity.java
+++ app/src/main/java/fr/enssat/lnfl/enrichedvideo/MainActivity.java
@@ -1,18 +1,10 @@
 package fr.enssat.lnfl.enrichedvideo;
 
-import android.media.MediaPlayer;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.WindowManager;
-import android.widget.RelativeLayout;
-import android.widget.VideoView;
-
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,18 +13,5 @@ public class MainActivity extends AppCompatActivity {
 
         //Remove the notification bar (full screen)
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-
-
-        String pathVideo="https://www.youtube.com/watch?v=p9vinmVUoy4";
-        Uri uriVideo = Uri.parse(pathVideo);
-
-        RelativeLayout rl = findViewById(R.id.rlID);
-
-        VideoView videoView = findViewById(R.id.videoViewID);
-        videoView.setVideoURI(uriVideo);
-
-        //rl.addView(videoView);
-
     }
 }
